03b8de960e2837a252a416e2119963d28f339982
mellalux <mella@lux.ee>
2023-07-20 19:21:52 +0300

    Update...

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import Bootstrap from 'bootstrap'
 
-createApp(App).mount('#app')
-
-import 'bootstrap/dist/js/bootstrap.js'
+createApp(App).use(Bootstrap).mount('#app')
